30c81a593b737617ffcb5ba5dd803d293f5184c6
bartug <bartugsevindik@gmail.com>
2019-07-31 14:15:34 +0300

    Mvc

---
+++ MvcProje/Controllers/SatısController.cs
@@ -0,0 +1,17 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProje.Controllers
+{
+    public class SatısController : Controller
+    {
+        // GET: Satıs
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
